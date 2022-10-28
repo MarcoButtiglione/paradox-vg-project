389c63bbadab3b731b37b59efb756aabaddcf1ac
Marco Buttiglione <marco.buttiglione98@gmail.com>
2022-10-28 13:19:26 +0200

    Audio manager and some sounds

--- game/paradox/Assets/Scripts/Controllers/CharacterController2D.cs
+++ game/paradox/Assets/Scripts/Controllers/CharacterController2D.cs
@@ -130,6 +130,8 @@ public class CharacterController2D : MonoBehaviour
             // Add a vertical force to the player.
             m_Grounded = false;
             m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+            //Play the jump sound
+            FindObjectOfType<AudioManager>().Play("Jump");
         }
     }
 
