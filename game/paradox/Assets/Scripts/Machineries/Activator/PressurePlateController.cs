987052d0a44d12a026e4bd0dcfcd3808c967e720
Marco Buttiglione <marco.buttiglione98@gmail.com>
2022-11-21 18:29:34 +0100

    Pressure plate done

--- game/paradox/Assets/Scripts/Machineries/Activator/PressurePlateController.cs
+++ game/paradox/Assets/Scripts/Machineries/Activator/PressurePlateController.cs
@@ -9,19 +9,104 @@ public class PressurePlateController : MonoBehaviour
     [SerializeField]private float _plateMovement = 0.05f;
     [SerializeField] private GameObject[] _objToActivate;
     
+    [Header("Initial state (Young/Old phase)")]
+    [SerializeField] private bool _initYoungState;
+    [SerializeField] private bool _initOldState;
+    
     
     private Vector3 _originalPos;
     private bool _moveBack;
     private bool _isActive = false;
+    private bool _isHover=false;
 
-    // Start is called before the first frame update
     void Start()
     {
         _originalPos = transform.position;
     }
-    private void OnTriggerStay2D(Collider2D collision)
+    //Functions used by the "activator manager". Initialize the activator in the various phases of the game.
+    public void InitYoung()
     {
-        if (collision.transform.CompareTag("Old"))
+        if (_initYoungState)
+        {
+            _isActive = true;
+            _isHover = true;
+        }
+        else
+        {
+            _isActive = false;
+            _isHover = false;
+        }
+        
+    }
+    public void InitOld()
+    {
+        if (_initOldState)
+        {
+            _isActive = true;
+            _isHover = true;
+        }
+        else
+        {
+            _isActive = false;
+            _isHover = false;
+        }
+    }
+    //-------------------------------
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Old"))  
+        {
+            _isHover = true;
+            col.gameObject.transform.SetParent(transform);
+            
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Old"))  
+        { 
+            _isHover = false;
+            col.gameObject.transform.SetParent(null); 
+        }    
+    }
+
+    private void SetActive()
+    {
+        _isActive = true;
+        for (int i = 0; i < _objToActivate.Length; i++) 
+        {
+            if (_objToActivate[i].GetComponent<MovingPlatformController>())
+            {
+                _objToActivate[i].GetComponent<MovingPlatformController>().SwitchState();
+            }
+            else
+            {
+                _objToActivate[i].SetActive(!_objToActivate[i].activeSelf);
+            }
+               
+        }
+    }
+    private void SetInactive()
+    {
+        _isActive = false;
+        for (int i = 0; i < _objToActivate.Length; i++) 
+        {
+            if (_objToActivate[i].GetComponent<MovingPlatformController>())
+            {
+                _objToActivate[i].GetComponent<MovingPlatformController>().SwitchState();
+            }
+            else
+            {
+                _objToActivate[i].SetActive(!_objToActivate[i].activeSelf);
+            }
+               
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        if (_isHover)
         {
             if (Math.Abs(transform.position.y-_originalPos.y)< _plateMovement)
             {
@@ -31,49 +116,11 @@ public class PressurePlateController : MonoBehaviour
             {
                 if (!_isActive)
                 {
-                    _isActive = true;
-                    for (int i = 0; i < _objToActivate.Length; i++) 
-                    {
-                        if (_objToActivate[i].GetComponent<MovingPlatformController>())
-                        {
-                            _objToActivate[i].GetComponent<MovingPlatformController>().SwitchState();
-                        }
-                        else
-                        {
-                            _objToActivate[i].SetActive(!_objToActivate[i].activeSelf);
-                        }
-               
-                    }
+                    SetActive();
                 }   
             }
-            _moveBack = false;
         }
-    }
-
-    
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.transform.CompareTag("Old"))
-        {
-            collision.transform.parent = transform;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.transform.CompareTag("Old"))
-        {
-            _moveBack = true;
-            collision.transform.parent = null;
-        }    
-    }
-    
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (_moveBack)
+        else
         {
             if (transform.position.y < _originalPos.y)
             {
@@ -83,21 +130,8 @@ public class PressurePlateController : MonoBehaviour
             {
                 if (_isActive)
                 {
-                    for (int i = 0; i < _objToActivate.Length; i++) 
-                    {
-                        if (_objToActivate[i].GetComponent<MovingPlatformController>())
-                        {
-                            _objToActivate[i].GetComponent<MovingPlatformController>().SwitchState();
-                        }
-                        else
-                        {
-                            _objToActivate[i].SetActive(!_objToActivate[i].activeSelf);
-                        }
-               
-                    }
-                    _isActive = false;
+                    SetInactive();
                 }
-                
                 _moveBack = false;
             }
         }
