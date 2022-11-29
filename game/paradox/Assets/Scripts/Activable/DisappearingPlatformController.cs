c6af9bf7ba5a54e662f64259f465ca0ad4c117f1
Marco Buttiglione <marco.buttiglione98@gmail.com>
2022-11-29 11:08:05 +0100

    Intermittent platform

--- game/paradox/Assets/Scripts/Activable/DisappearingPlatformController.cs
+++ game/paradox/Assets/Scripts/Activable/DisappearingPlatformController.cs
@@ -7,97 +7,150 @@ using UnityEngine;
 public class DisappearingPlatformController : MonoBehaviour
 {
     [SerializeField] private DisappearingFunctioning _function;
+
     [Tooltip("Set the period if function is 'Intermittent'. Set function 'Intermittent'.")]
-    [HideInInspector][Range(0.0f, 10.0f)] public float _intermittentPeriod=0f;
-    [Header("Initial state (Young/Old phase)")]
-    [SerializeField] private DisappearingState _initYoungState;
+    [Range(0.0f, 10.0f)]
+    [SerializeField]
+    private float _intermittentPeriod = 1f;
+    [Range(0.0f, 10.0f)]
+    [SerializeField]
+    private float _intermittentDelay = 1f;
+
+    private bool _isIntermittent;
+
+    [Header("Initial state (Young/Old phase)")] [SerializeField]
+    private DisappearingState _initYoungState;
+
     [SerializeField] private DisappearingState _initOldStateOFF;
 
-    [Header("Sprites")]
-    [SerializeField] private Sprite _spriteOff;
+    [Header("Sprites")] [SerializeField] private Sprite _spriteOff;
     [SerializeField] private Sprite _spriteOn;
     private SpriteRenderer _spriteRenderer;
     private Collider2D _collider2D;
-    
+
     private bool _isActive;
-    
-    
+
+
     //-------------------------------
     private void Awake()
     {
-        _spriteRenderer=gameObject.GetComponent<SpriteRenderer>();
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         _collider2D = gameObject.GetComponent<Collider2D>();
         //It is subscribing to the event
         GameManager.OnGameStateChanged += GameManagerOnGameStateChanged;
     }
+
     private void OnDestroy()
     {
         //It is unsubscribing to the event
         GameManager.OnGameStateChanged -= GameManagerOnGameStateChanged;
     }
+
     private void GameManagerOnGameStateChanged(GameState state)
     {
         if (state == GameState.StartingYoungTurn)
         {
             InitYoung();
         }
+
         if (state == GameState.StartingOldTurn)
         {
             InitOld();
         }
     }
+
     private void InitYoung()
     {
-        StopAllCoroutines();
-        if (_initYoungState == DisappearingState.Active)
+        CancelInvoke();
+        if (_function == DisappearingFunctioning.Fixed)
         {
-            SetActive();
+            if (_initYoungState == DisappearingState.Active)
+            {
+                SetActive();
+            }
+            else if (_initYoungState == DisappearingState.Inactive)
+            {
+                SetInactive();
+            }
         }
-        else if (_initYoungState==DisappearingState.Inactive)
+        else if (_function == DisappearingFunctioning.Intermittent)
         {
-            SetInactive();
+            if (_initYoungState == DisappearingState.Active)
+            {
+                _isIntermittent = true;
+                SetActive();
+                InvokeRepeating("Intermittent", _intermittentPeriod / 2+_intermittentDelay, _intermittentPeriod / 2);
+            }
+            else if (_initYoungState == DisappearingState.Inactive)
+            {
+                _isIntermittent = false;
+                CancelInvoke();
+                SetInactive();
+            }
         }
     }
+
     private void InitOld()
     {
-        StopAllCoroutines();
-        if (_initOldStateOFF == DisappearingState.Active)
+        CancelInvoke();
+        if (_function == DisappearingFunctioning.Fixed)
         {
-            SetActive();
+            if (_initOldStateOFF == DisappearingState.Active)
+            {
+                SetActive();
+            }
+            else if (_initOldStateOFF == DisappearingState.Inactive)
+            {
+                SetInactive();
+            }
         }
-        else if (_initOldStateOFF==DisappearingState.Inactive)
+        else if (_function == DisappearingFunctioning.Intermittent)
         {
-            SetInactive();
+            if (_initOldStateOFF == DisappearingState.Active)
+            {
+                _isIntermittent = true;
+                SetActive();
+                InvokeRepeating("Intermittent", _intermittentPeriod / 2+_intermittentDelay, _intermittentPeriod / 2);
+            }
+            else if (_initOldStateOFF == DisappearingState.Inactive)
+            {
+                _isIntermittent = false;
+                CancelInvoke();
+                SetInactive();
+            }
         }
     }
     //-------------------------------
 
-    /*
-    private IEnumerator Intermittent()
-    {
-        while (_isActive)
-        {
-            Debug.Log("CORUTINE");
-            gameObject.SetActive(false);
-            yield return new WaitForSeconds(_intermittentPeriod/2);
-            gameObject.SetActive(true);
-            yield return new WaitForSeconds(_intermittentPeriod/2);
-        }
-        
-    }
-    */
+
     public void SwitchState()
     {
-        if (_isActive)
+        if (_function == DisappearingFunctioning.Fixed)
         {
-            SetInactive();
-            StopAllCoroutines();
+            if (_isActive)
+            {
+                SetInactive();
+            }
+            else
+            {
+                SetActive();
+            }
         }
-        else
+        else if (_function == DisappearingFunctioning.Intermittent)
         {
-            SetActive();
+            if (_isIntermittent)
+            {
+                _isIntermittent = false;
+                CancelInvoke();
+                SetInactive();
+            }
+            else
+            {
+                _isIntermittent = true;
+                SetActive();
+                InvokeRepeating("Intermittent", _intermittentPeriod / 2, _intermittentPeriod / 2);
+            }
         }
-        
     }
 
 
@@ -105,10 +158,7 @@ public class DisappearingPlatformController : MonoBehaviour
     {
         _spriteRenderer.sprite = _spriteOn;
         _isActive = true;
-        if (_function == DisappearingFunctioning.Fixed)
-        {
-            _collider2D.enabled = true;            
-        }
+        _collider2D.enabled = true;
     }
 
     private void SetInactive()
@@ -116,6 +166,24 @@ public class DisappearingPlatformController : MonoBehaviour
         _spriteRenderer.sprite = _spriteOff;
         _isActive = false;
         _collider2D.enabled = false;
+    }
+
+
+    private void Intermittent()
+    {
+        if (GameManager.Instance.State is not GameState.StartingYoungTurn or GameState.StartingSecondPart
+            or GameState.StartingThirdPart or GameState.StartingOldTurn)
+        {
+            _isActive = !_isActive;
+            if (_isActive)
+            {
+                SetActive();
+            }
+            else
+            {
+                SetInactive();
+            }
+        }
     }
 }
 
@@ -126,10 +194,10 @@ public enum DisappearingState
     Inactive,
     Active
 }
+
 [System.Serializable]
 public enum DisappearingFunctioning
 {
     Fixed,
-    //Intermittent
+    Intermittent
 }
-
